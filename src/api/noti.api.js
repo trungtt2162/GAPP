e12cb1034b607c7b2a2da2a203c2d4b9759b00b1
trung <trungtt.212@gmail.com>
2024-05-18 01:05:04 +0700

    fix noti

--- src/api/noti.api.js
+++ src/api/noti.api.js
@@ -4,4 +4,7 @@ export const notiApi = {
   getListNoti: (id, txt = "") => {
     return API.get("/api/User/notification");
   },
+  viewNoti:(data) => {
+    return API.put("/api/User/notification/view",data)
+  }
 };
